1727787e221fa679a0b346384eb3eb82ef954392
adamprzybyla <adam.przybyla@gmail.com>
2018-07-06 14:32:02 +0200

    test_doc.py for pyfleks tests cleanup

--- test_doc.py
+++ test_doc.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python
-from test.test_support import run_unittest
 from doctest import DocFileSuite
 import unittest
-import cf as math
 
 def load_tests(loader, standard_tests, pattern):
     return DocFileSuite("ieee754.txt")
